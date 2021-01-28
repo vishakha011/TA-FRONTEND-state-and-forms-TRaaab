7d159c95f9814774d73ccd1028fa30742e5ab17b
vishakha011 <khatadevishakha01@gmail.com>
2021-01-28 19:32:03 +0530

    Handling State

---
+++ block-BRaabd/code/image-app/src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Buttons from "./imageButton";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <Buttons />
+        )
+    
+    }
+       
+}
+
+export default App;
