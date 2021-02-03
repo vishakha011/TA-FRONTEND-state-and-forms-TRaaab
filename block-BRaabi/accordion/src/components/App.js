79c9f608d2ab3d07d81ade31ebaf419787bfdeb0
vishakha011 <khatadevishakha01@gmail.com>
2021-02-03 22:44:16 +0530

    Accordion

---
+++ block-BRaabi/accordion/src/components/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Accordions from "../components/Accordion"
+
+class App extends React.Component {
+    render() {
+        return (
+            <>
+            <header>
+                <h1>FAQ</h1>
+            </header>
+            <main>
+            < Accordions />
+            </main>
+                
+            </>
+        )
+    }
+}
+
+export default App;
---
+++ block-BRaabi/accordion/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+import App from "./components/App";
+import "./styles/main.css"
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+ReactDom.render(<App />, document.getElementById("root"));
